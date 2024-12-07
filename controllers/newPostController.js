f39657e797579185f49caa4d52e1f1350d242e9b
Samith-hegde <hegdesamith02@gmail.com>
2024-12-07 11:32:50 +0530

    Add form validation to newPost form

--- controllers/newPostController.js
+++ controllers/newPostController.js
@@ -1,13 +1,32 @@
 const prisma = require('../config/prismaClient');
+const { body, validationResult } = require('express-validator');
+
+const validateUser = [
+  body('title').trim()
+    .isLength({ min: 1 }).withMessage('Title must not be empty.')
+    .isLength({ max: 25 }).withMessage('Title must not be more than 25 characters long.'),
+  body('textContent').trim()
+    .isLength({ min: 1 }).withMessage('Text content must not be empty.')
+    .isLength({ max: 1000 }).withMessage('Text content must not be more than 1000 characters long.'),
+]
 
 const newPostForm = async (req, res) => {
     const categories = await prisma.categories.findMany();
 
-    res.render('newPost', { title: 'New Post', user: req.user, categories });
+    res.render('newPost', { title: 'New Post', user: req.user, categories, errors: [] });
 };
 
-const createPost = async (req, res) => {
+const createPost = [
+  validateUser,
+  async (req, res) => {
     try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+          const categories = await prisma.categories.findMany();
+          return res.status(400).render('newPost', { title: 'New Post', user: req.user, categories, errors: errors.array() });
+        }
+
         const { title, textContent, category, newCategory } = req.body;
         let categoryId = null;
     
@@ -36,6 +55,7 @@ const createPost = async (req, res) => {
         console.error(error);
         res.send('An error occurred: ' + error);
     }
-}
+  }
+];
 
 module.exports = { newPostForm, createPost };
